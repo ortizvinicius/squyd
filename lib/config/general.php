b6326229877f390f1d03ba86f2fd5a093bf6862c
ortizvinicius <vinicius.oaw@gmail.com>
2016-08-20 15:06:53 -0300

    select function

---
+++ lib/config/general.php
@@ -0,0 +1,13 @@
+<?php 
+
+//Database
+define("DBCONN", "mysql:
+				  host=localhost;
+				  dbname=admin");
+define("DBUSER", "admin");
+define("DBPASS", "");
+
+//Time
+date_default_timezone_set('America/Sao_Paulo');
+
+?>
--- lib/dataHandling/dbAccess.php
+++ lib/dataHandling/dbAccess.php
@@ -4,13 +4,23 @@ namespace squyd;
 
 class DbAccess {
     
-  private $conex;
+  protected $conex,
+            $tableName,
+            $tableFields;
   
-  function __construct () {
+  /* @param {string} tableName
+   * @param {array} tableFields
+   */
+  function __construct ($tableName, $tableFields) {
+    
     $this->conex = new PDO(DBCONN, DBUSER, DBPASS);
+
+    $this->tableName = $tableName;
+    $this->tableFields = $tableFields;
+
   }
   
-  public function execQuery ($query, $params, $uni == null) {
+  public function execQuery ($query, $params, $single = null) {
     try {  
       $sqlQuery = $this->conex->prepare($query);
       
@@ -21,9 +31,9 @@ class DbAccess {
       
       $sqlQuery->execute();
       
-      if($uni != null && $uni){
+      if($single != null && $single){
         return $sqlQuery->fetch();   
-      } else if($uni != null) {
+      } else if($single != null) {
         return $sqlQuery->fetchAll();
       } else {
         return true;
@@ -33,7 +43,72 @@ class DbAccess {
     }
   }
 
-  public function select () {
+  /* @param {array} options
+   */
+  protected function optionsSet ($options) {
+    $options["fields"] = isset($options["fields"]) && $options["fields"] ? $options["fields"] : $this->tableFields;
+    $options["join"]   = isset($options["join"])   && $options["join"]   ? $options["join"]   : [];
+    $options["where"]  = isset($options["where"])  && $options["where"]  ? $options["where"]  : [];
+    $options["order"]  = isset($options["order"])  && $options["order"]  ? $options["order"]  : "";
+    $options["group"]  = isset($options["group"])  && $options["group"]  ? $options["group"]  : "";
+    $options["limit"]  = isset($options["limit"])  && $options["limit"]  ? $options["limit"]  : "";
+    $options["single"] = isset($options["single"]) && $options["single"] ? $options["single"] : false;
+
+    return $options;
+  }
+
+  /* @param {array} options
+   */
+  public function select ($options = []) {
+
+    $options = $this->optionsSet($options);
+
+    $lastFieldsKey = array_pop(array_keys($options["fields"]));
+
+    $params = [];
+
+    $query = "SELECT ";
+
+    foreach ($options["fields"] as $key => $field) {
+      $query .= $field;
+      $query .= $key != $lastFieldsKey ? ", " : "";
+    }
+
+    $query .= " FROM " . $this->tableName;
+
+    foreach ($options["join"] as $type => $join) {
+      $query .= " " . $type . " JOIN ";
+      $query .= $join["table"] . " ON (";
+      $query .= $join["on"] . ") ";
+    }
+
+    foreach ($options["where"] as $where) {
+      $query .= $where["field"] . " " . $where["cond"] . " ? ";
+      array_push($params, $where["value"]);
+    }
+
+    if($options["order"]){
+      $query .= " ORDER BY ? ";
+      array_push($params, $options["order"]);
+    }
+
+    if($options["group"]){
+      $query .= " GROUP BY ? ";
+      array_push($params, $options["group"]);
+    }
+
+    if($options["limit"]){
+      if(isset($options["limit"][0]) && isset($options["limit"][1])){
+        $query .= " LIMIT ?, ? ";
+        array_push($params, $options["limit"][0]);
+        array_push($params, $options["limit"][1]);
+      }
+    } else {
+      $query .= " LIMIT ? ";
+      array_push($params, $options["limit"]);
+    }
+
+    return $this->execQuery($query, $params, $options["single"]);
 
   }
 
